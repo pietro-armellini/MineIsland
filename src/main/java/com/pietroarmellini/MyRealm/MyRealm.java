d791348422977caae82fd491a7627960db46a243
pietro-armellini <pietro.armellini@studenti.unitn.it>
2025-09-14 19:29:31 +0200

    Initial Commit 2

---
+++ src/main/java/com/pietroarmellini/MyRealm/MyRealm.java
@@ -0,0 +1,11 @@
+package com.pietroarmellini.MyRealm;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyRealm extends JavaPlugin implements Listener {
+  @Override
+  public void onEnable() {
+    System.out.println("MyRealm plugin has been enabled!");
+	}
+}
